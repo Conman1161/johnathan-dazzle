bf508181fe46904d0ff44a49d12ec9d0c21e34b3
Conman1161 <56032728+Conman1161@users.noreply.github.com>
2021-01-26 23:15:20 -0500

    Update Catboy mode

--- commands/general/info.js
+++ commands/general/info.js
@@ -1,7 +1,8 @@
 const commando = require("discord.js-commando");
 const discord = require("discord.js");
 const { version } = require("../../package.json");
-const catboy = false;
+const { catboy } = require('../../config.json');
+const owoify = require('owoifyx');
 const attachment = new discord.MessageAttachment(
   catboy ? "./images/catboy/Background.png" : "./images/icon.png",
   "icon.png"
@@ -23,26 +24,28 @@ class BotInfoCommand extends commando.Command {
   async run(message) {
     message.channel.startTyping();
     var myInfo = new discord.MessageEmbed()
-      .setAuthor("Johnathan Dazzle", message.client.user.displayAvatarURL({ dynamic: true }))
+      .setAuthor(`${catboy ? owoify('Johnathan Dazzle') : 'Johnathan Dazzle'}`, message.client.user.displayAvatarURL({ dynamic: true }))
       .addField(
-        "Bot Info",
-        "Hi, I'm Johnathon Dazzle! I roll the dice around here. I've got a few tricks up my sleeve, so check them out with `!help`!"
+        `${catboy ? `${owoify('Bot Info')}` : `Bot Info`}`,
+        `${catboy ? `${owoify(`Hello, I'm Johnathon Dazzle! I roll the dice around here. I've got a few tricks up my sleeve, so check them out with`)}` : `Hello, I'm Johnathon Dazzle! I roll the dice around here. I've got a few tricks up my sleeve, so check them out with`} \`!help\`!`
       )
       .addField(
-        "__**WORK IN PROGRESS**__",
-        "Dazzle is currently in a **Work in Progress** state, meaning some functions and features are not finished or completely implemented yet. Please keep this in mind when using Dazzle"
+        `${catboy ? `${owoify(`__**WORK IN PROGRESS**__`)}` : `__**WORK IN PROGRESS**__`}`,
+        `${catboy ? `${owoify(`Dazzle is currently in a **Work in Progress** state, meaning some functions and features are not finished or completely implemented yet. Please keep this in mind when using Dazzle`)}` : `Dazzle is currently in a **Work in Progress** state, meaning some functions and features are not finished or completely implemented yet. Please keep this in mind when using Dazzle`}`
       )
       .addField(
-        "Bot Support",
-        `If you find an issue, message __**${message.client.owners[0].tag}**__ with a screenshot and a short description of the issue`,
+        `${catboy ? `${owoify(`Bot Support`)}` : `Bot Support`}`,
+        `${catboy ? `${owoify(`If you find an issue, message __**${message.client.owners[0].tag}**__ with a screenshot and a short description of the issue`)}` : `If you find an issue, message __**${message.client.owners[0].tag}**__ with a screenshot and a short description of the issue`}`,
         true
       )
       .addField(
-        "Command Ideas",
-        `If you have any ideas for commands, message __**${message.client.owners[0].tag}**__ with them`,
+        `${catboy ? `${owoify(`Command Ideas`)}` : `Command Ideas`}`,
+        `${catboy ? `${owoify(`If you have any ideas for commands, message __**${message.client.owners[0].tag}**__ with them`)}` : `If you have any ideas for commands, message __**${message.client.owners[0].tag}**__ with them`}`,
         true
       )
-      .addField("Bot Version", `Currently running v**${version}**`)
+      .addField(
+        `${catboy ? `${owoify(`Bot Version`)}` : `Bot Version`}`,
+        `${catboy ? `${owoify(`Currently running v**${version}**`)}` : `Currently running v**${version}**`}`)
       .addField(
         "Artwork/Assets",
         'All artwork has been commissioned and made by **Kai**. They can be found on [`Twitter`](https://twitter.com/ckttle_ "@ckttle_") and [`Instagram`](https://instagram.com/ckttle "@ckttle").'
