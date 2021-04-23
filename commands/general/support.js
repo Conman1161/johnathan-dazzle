02aa6837b48e25e79822b2786fb3f3f19e32bfcc
Conman1161 <56032728+Conman1161@users.noreply.github.com>
2021-04-22 20:26:57 -0400

    Support refactor

--- commands/general/support.js
+++ commands/general/support.js
@@ -1,43 +1,45 @@
-const commando = require("discord.js-commando");
-const discord = require("discord.js");
-const attachment = new discord.MessageAttachment(
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const { readFileSync } = require("fs");
+const { SlashCommand } = require("slash-create");
+const { ownerTag } = require('../../config.json');
+const attachment = new MessageAttachment(
   "./images/support.png",
   "support.png"
 );
 
-class BotInfoCommand extends commando.Command {
+class BotInfoCommand extends SlashCommand {
   constructor(client) {
     super(client, {
-      aliases: [],
       description: "Support information for Dazzle",
-      examples: ["!support"].sort(),
-      format: "",
-      group: "general",
-      memberName: "support",
       name: "support",
     });
   }
 
-  async run(message) {
-    message.channel.startTyping();
-    var embed = new discord.MessageEmbed()
-      .setAuthor("Support Information", message.client.user.displayAvatarURL({ dynamic: true }))
+  async run(ctx) {
+    await ctx.defer();
+    var embed = new MessageEmbed()
+      .setAuthor("Support Information", `https://cdn.discordapp.com/avatars/${ctx.user.id}/${ctx.user.avatar}.png`)
       .addField(
         "Support Server",
         `The link to the support Discord server can be found [here](https://discord.gg/ZUJAMnh "Click me, I go to the server!")`
       )
       .addField(
         "Need more help?",
-        `Contact ${message.client.owners[0].tag} if you have any further questions`
+        `Contact ${ownerTag} if you have any further questions`
       )
       .setColor("#fe00ff")
       .attachFiles([attachment])
       .setURL("https://discord.gg/ZUJAMnh")
       .setThumbnail("attachment://support.png");
 
-    message.channel.send(embed);
-    message.channel.stopTyping();
+    ctx.send({
+      embeds: [embed],
+      file: {
+        name: 'support.png',
+        file: readFileSync(attachment.attachment)
+      }
+    })
   }
 }
 
-// module.exports = BotInfoCommand;
+module.exports = BotInfoCommand;
