80ffcc814f8bc2d66efefccf6cb98732284a05dd
Conman1161 <56032728+Conman1161@users.noreply.github.com>
2021-05-24 23:42:05 -0500

    Convert General Commands

---
+++ commands/general/example.ts
@@ -0,0 +1,54 @@
+import { MessageEmbed } from "discord.js";
+import { readFileSync } from "fs";
+import { SlashCommand, CommandOptionType, SlashCreator, CommandContext } from "slash-create";
+const errorMod = require('../modules/error');
+// const { hostGuildID } = require('../../config.json');
+
+class ExampleCommand extends SlashCommand {
+   constructor(creator: SlashCreator) {
+      super(creator, {
+         description: "A template for commands",
+         name: "example",
+         options: [{
+            name: 'exampleOne',
+            description: `Here's an example option with choices`,
+            type: CommandOptionType.STRING,
+            choices: [{
+               name: 'Choice One',
+               value: 'I can be lots of things!',
+            }].sort((a, b) => (a.name > b.name) ? 1 : -1),
+         }],
+         // guildIDs: [hostGuildID]
+      });
+      this.filePath = __filename;
+   }
+
+   async run(ctx: CommandContext) {
+      try {
+         await ctx.defer();
+         let embed = new MessageEmbed().setColor('RANDOM');
+         embed.setAuthor(`An example title!`, `https://cdn.discordapp.com/avatars/${ctx.user.id}/${ctx.user.avatar}.png`);
+         return {
+            embeds: [embed],
+            file: {
+               name: `fileName.png`,
+               file: readFileSync(`filePath`)
+            }
+         };
+      } catch (err) {
+         await ctx.send({
+            embeds: [errorMod.errorMessage(err, ctx)],
+            file: {
+               name: `error.png`,
+               file: readFileSync(`./images/error.png`)
+            }
+         });
+      } finally {
+      }
+   }
+   async onError(err: any, ctx: CommandContext) {
+      await ctx.send(`An error occurred! Here is the message: \`${err}\``);
+   }
+}
+
+// module.exports = ExampleCommand;
--- commands/general/restart.js
+++ commands/general/restart.js
@@ -1,26 +1,26 @@
-const { SlashCommand } = require("slash-create");
-const { hostGuildID, owner } = require('../../config.json');
-
-class RestartCommand extends SlashCommand {
-  constructor(client) {
-    super(client, {
-      description: "Restart the bot. Only available to the bot owner",
-      name: "restart",
-      guildIDs: [hostGuildID]
-    });
-    this.filePath = __filename;
-  }
-
-  async run(ctx) {
-    await ctx.defer();
-    if (ctx.user.id === owner) {
-      await ctx.send("Restarting...");
-      //pm2 on host machine to auto-restart, this will simply end the bot process otherwise
-      process.exit(0);
-    } else {
-      await ctx.send('`You do not have permission to run this command!`');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const slash_create_1 = require("slash-create");
+const config_json_1 = require("../../config.json");
+class RestartCommand extends slash_create_1.SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            description: "Restart the bot. Only available to the bot owner",
+            name: "restart",
+            guildIDs: [config_json_1.hostGuildID]
+        });
+        this.filePath = __filename;
     }
-  }
+    async run(ctx) {
+        await ctx.defer();
+        if (ctx.user.id === config_json_1.owner) {
+            await ctx.send("Restarting...");
+            //pm2 on host machine to auto-restart, this will simply end the bot process otherwise
+            process.exit(0);
+        }
+        else {
+            await ctx.send('`You do not have permission to run this command!`');
+        }
+    }
 }
-
 module.exports = RestartCommand;
